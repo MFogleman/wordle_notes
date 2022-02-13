ff0a05d8310c7a900509886f39c32b3b5444a719
Marc Fogleman <Marcfogleman@gmail.com>
2022-02-13 10:38:25 -0500

    Create notes.py

---
+++ notes.py
@@ -0,0 +1,33 @@
+from functools import reduce
+
+WORDS = ["apple", "bands", "carat", "doggo", "eaten", "foxes", "goats", "hoppy", "kites", "lamer"]
+
+def score_letters_by_count(words):
+  d = {}
+  for word in words:
+    for letter in word:
+      if letter in d.keys():
+        d[letter] += 1
+      else:
+        d[letter] = 1
+  return d
+
+
+def score_word(score_val, word):
+  score = 0
+  for letter in word:
+    score += score_val[letter]
+  return score
+
+def make_guess(words):
+  d = score_letters_by_count(words)
+  top_word = None
+  top_score = 0
+  for word in words:
+    new_score = score_word(d, word) 
+    if  new_score > top_score:
+      top_word = word
+      top_score = new_score
+  print(f"top_score: {top_score}, top_word: {top_word}")
+
+make_guess(WORDS)
